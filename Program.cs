82a33cc6e46616f23eb2deec73017f90b219ca5e
Flora <114140834+Floraxoxo@users.noreply.github.com>
2023-03-23 11:16:07 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿if (args.Length == 2) {
+    try
+    {
+        Iban input = new (args[0], args[1]); Console.WriteLine($"{input.BuildIban()}");
+    }
+    catch (ArgumentException ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
+else if (args.Length > 2) {Console.WriteLine("Argument is to long. Please try again!");}
+else {Console.WriteLine("Argument is to short. Please try again!");}
